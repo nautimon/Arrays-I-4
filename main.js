b158af8a7896fbc2d6e6f192edd3b0b13b8463b6
nautimon <nautisignup@gmail.com>
2026-06-11 01:09:57 +0000

    Modify array with splice

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 var ranks = ["domain", "kingdom", "category", "family", "genus"];
 
 // Write your code below
+ranks.splice(5,0, "species");
+ranks.splice(2,1);
+ranks.splice(2, 0, "phylum", "class", "order");
+
+
+console.log(ranks);
